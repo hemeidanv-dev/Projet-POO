e48581994e57e68535e2b4afa1b306cc71ecdc3f
alesjoanmay-bit <alesjoan.may@gmail.com>
2026-05-05 15:14:21 +0000

    hydro

---
+++ Hydro.java
@@ -0,0 +1,29 @@
+public class Hydro extends Mage {
+    public Hydro(int x, int y) {
+        super(x, y, Element.EAU);
+    }
+
+    @Override
+    public int calculerPoints(String[][] grille) {
+        int points = 0;
+        int n = grille.length;
+
+        //LIGNE
+        for (int j = 0; j < n; j++) {
+            if (j == y) continue;
+            if (Element.EAU.equals(grille[x][j])) {
+                points++;
+            }
+        }
+
+        //COLONNE
+        for (int i = 0; i < n; i++) {
+            if (i == x) continue;
+            if (Element.EAU.equals(grille[i][y])) {
+                points++;
+            }
+        }
+
+        return points;
+    }
+}
